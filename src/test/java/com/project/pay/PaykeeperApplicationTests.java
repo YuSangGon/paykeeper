1895c106820a52bb1665b9626f6f43cc424fc30c
yusanggon <yusang5159@naver.com>
2024-01-09 21:56:58 +0900

    create project

---
+++ src/test/java/com/project/pay/PaykeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.pay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaykeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
