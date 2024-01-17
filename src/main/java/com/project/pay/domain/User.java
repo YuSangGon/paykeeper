de14ddae6e8c38c63c72381829a3d0472f15c45b
dusdnr33 <qewrytu@naver.com>
2024-01-17 16:14:17 +0900

    Modified Entity

--- src/main/java/com/project/pay/domain/User.java
+++ src/main/java/com/project/pay/domain/User.java
@@ -1,4 +1,24 @@
 package com.project.pay.domain;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@Entity(name = "p_user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String email;
+
+    @OneToMany(mappedBy = "p_user_id")
+    private List<Job> jobs = new ArrayList<>();
 }
